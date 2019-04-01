b5c6bbb51ec68675d70c904712ffe2402719b1fd
karma <karmadrukpa11@gmail.com>
2019-04-01 16:45:38 -0700

    added routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send("hello karma")
+  res.send("This is just a HOME route Karma!!! peace")
 });
 
 
