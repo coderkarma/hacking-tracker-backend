5cf5333cd5ae2f9f1bc448a8a435e516d3cdbfb1
Karma Drukpa <karmadrukpa11@gmail.com>
2020-12-23 20:58:31 -0500

    Remove outdated frontend files

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../models')
-const path = require('path')
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  console.log('zzzzzzzzzzz');
-  res.sendFile(path.join(__dirname, "../public/build", "index.html"));
-});
+const db = require('../models');
+const path = require('path');
 
+router.get('/', function (req, res, next) {
+	res.json({});
+});
 
 module.exports = router;
